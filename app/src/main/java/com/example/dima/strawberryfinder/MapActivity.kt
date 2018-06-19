86638983875b97df3573ba81ee711b74cabf09ab
kisate <dhkas@mail.ru>
2018-06-19 15:21:19 +0300

    positioning

--- app/src/main/java/com/example/dima/strawberryfinder/MapActivity.kt
+++ app/src/main/java/com/example/dima/strawberryfinder/MapActivity.kt
@@ -18,6 +18,7 @@ import java.util.*
 import android.R.attr.y
 import android.R.attr.x
 import android.graphics.Point
+import android.graphics.PorterDuff
 import android.view.Display
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseError
@@ -29,17 +30,19 @@ import com.google.firebase.database.ValueEventListener
 
 class MapActivity : FullscreenActivity() {
 
-    class Berry(val x : Double, val y : Double, val id : Int, var status : Int, val price : Double)
+    class Berry(val x : Double, val y : Double, val id : Int, var status : Int, val price : Double, val encoder : Int)
 
 
     var width = 150.0
     var height = 40.0
     var berryWidth = width/20
     var pixToCm = 10.0
+    val encToCm = 4.1/360
     val xa = (width*pixToCm).toInt()
     val ya = (height*pixToCm).toInt()
     val berryes = mutableListOf<Berry>()
     val reference = FirebaseDatabase.getInstance().reference
+    val spacing = 0.05
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,7 +61,7 @@ class MapActivity : FullscreenActivity() {
 
                 height = field.child("height").value.toString().toDouble()
                 width = field.child("width").value.toString().toDouble()
-                berryWidth = width/10
+                berryWidth = width/20
 
                 val params = maplayout.layoutParams as FrameLayout.LayoutParams
                 //val scale = resources.displayMetrics.density
@@ -67,12 +70,16 @@ class MapActivity : FullscreenActivity() {
                 val size = Point()
                 display.getRealSize(size)
 
-                params.height = (size.y*0.8).toInt()
+                pixToCm = size.y/height*(1-spacing*2)
+
+                params.height = (size.y*(1-spacing*2)).toInt()
                 params.width = (width*pixToCm).toInt()
-                params.topMargin = (size.y*0.1).toInt()
+                params.topMargin = (size.y*spacing).toInt()
                 maplayout.layoutParams = params
 
-                berryData.children.forEach({child -> berryes.add(Berry(child.child("x").value.toString().toDouble(), child.child("y").value.toString().toDouble(), child.child("id").value.toString().toInt(), child.child("status").value.toString().toInt(), child.child("price").value.toString().toDouble()))})
+                berryes.clear()
+
+                berryData.children.forEach({child -> berryes.add(Berry(child.child("x").value.toString().toDouble(), child.child("y").value.toString().toDouble(), child.child("id").value.toString().toInt(), child.child("status").value.toString().toInt(), child.child("price").value.toString().toDouble(), child.child("encoder").value.toString().toInt()))})
 
                 maplayout.removeAllViews()
 
@@ -91,29 +98,45 @@ class MapActivity : FullscreenActivity() {
         val view = ImageView(this)
 
         view.setImageResource(R.drawable.strawberry)
-        view.scaleType = ImageView.ScaleType.CENTER
+        view.scaleType = ImageView.ScaleType.FIT_START
         view.adjustViewBounds = true
         //view.background = BitmapDrawable(Bitmap.createBitmap(intArrayOf(Color.BLACK), 1, 1, Bitmap.Config.RGB_565))
         val scale = resources.displayMetrics.density
 
 
         val params = RelativeLayout.LayoutParams((berryWidth*pixToCm).toInt(), RelativeLayout.LayoutParams.WRAP_CONTENT)
-        params.leftMargin = (berry.x*pixToCm).toInt()
+        params.leftMargin = (berry.encoder*encToCm*pixToCm).toInt()
         params.topMargin = (berry.y*pixToCm).toInt()
         maplayout.addView(view, params)
 
-        if (berry.status == 1) view.setColorFilter(Color.GRAY)
-        else if (berry.status == 2) view.setColorFilter(Color.GREEN)
-
-        view.setOnClickListener { displayDialog(view, berry) }
+        if (berry.status == 0) view.setOnClickListener { displayDialog1(view, berry) }
+        if (berry.status == 1) {
+            view.setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY)
+            view.setOnClickListener { displayDialog2(view, berry) }
+        }
+        else if (berry.status == 2) view.setColorFilter(Color.GREEN, PorterDuff.Mode.MULTIPLY)
     }
 
-    fun displayDialog(view : View, berry : Berry) {
+    fun displayDialog1(view : View, berry : Berry) {
         textView.text = "Price : ${berry.price}\n Buy?"
         button2.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 berry.status = 1
                 reference.child("berrys").child(berry.id.toString()).child("status").setValue("1")
+                buyLayout.visibility = View.GONE
+            }
+        })
+        button3.setOnClickListener { buyLayout.visibility = View.GONE }
+
+        buyLayout.visibility = View.VISIBLE
+    }
+
+    fun displayDialog2(view : View, berry : Berry) {
+        textView.text = "Price : ${berry.price}\n Remove from queue?"
+        button2.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                berry.status = 0
+                reference.child("berrys").child(berry.id.toString()).child("status").setValue("0")
                 buyLayout.visibility = View.GONE
             }
         })
